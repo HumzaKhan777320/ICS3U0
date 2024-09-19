40afd403177cbeb882ac2b25ae443a8ff4a79e31
HumzaKhan777320 <777320@pdsb.net>
2024-09-19 10:16:43 -0400

    Create Unit1Lesson4.py

---
+++ Unit1Lesson4.py
@@ -0,0 +1,5 @@
+num = input("Please input a whole number: ") 
+num = int(num) 
+num3 = int(input("Use any whole number except zero: "))
+num2 = num / num3
+print("When", num, " is divided by", num3, "the result is:", num2)
