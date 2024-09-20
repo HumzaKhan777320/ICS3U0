543e068224571eed3e3996f402b2a83dd60141d6
HumzaKhan777320 <777320@pdsb.net>
2024-09-20 09:10:38 -0400

    Create Unit1Lesson6.py

---
+++ Unit1Lesson6.py
@@ -0,0 +1,9 @@
+birth_year=int(input("Enter your year of birth")) #Variable to store birth year
+age = int(input("Enter your current age")) #Variable to store age
+birth_year*=2 #Multiplying birth year by 2
+birth_year+=5 #Adding 5 to previous calculation
+birth_year*=50 #Multiplying 50 to previous calculation
+birth_year+=age #adding users age to previous calculation
+birth_year-=250 #Subtracting 250 from previous calculation
+birth_year/=100 #Dividing previous calculation by 100
+print("When your birth year is doubled, gets 5 added to it, multiplyed by 50, plus your age, 250 subtracted, and then divided by 100 it's",birth_year) # printing new value of birth year and interpreting how it changed
