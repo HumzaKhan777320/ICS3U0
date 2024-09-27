73cfad4090e5dee9c73cde4f0745dd24d08689f9
HumzaKhan777320 <777320@pdsb.net>
2024-09-27 09:49:02 -0400

    Update Unit2Lesson1.py

--- Unit2Lesson1.py
+++ Unit2Lesson1.py
@@ -1,10 +1,10 @@
-print("Make a choice from the following menu: ")
+print("Make a choice from the following menu: ") #Giving the user instructions
 print("A: apples")
 print("B: bananas")
 print("C: cherries")
 
-ch = input("Your choice: ")
-
+ch = input("Your choice: ")#Giving the user instructions and taking input
+# Using if/else statements to give output based on user input
 if (ch == "A"):
     print("I prefer apples")
 elif (ch == "B"):
@@ -13,8 +13,9 @@ elif(ch=="C"):
     print("I prefer cherries")
 else: 
   print("Error input")
-
-x=int(input("Enter a number between 1 and 100: "))
+# Part 2 of the assignment
+x=int(input("Enter a number between 1 and 100: ")) #Taking input from user
+# Using if/else statements to give output based on user input
 if(x>=80 and x<=100):
   print("A")
 if(x>=70 and x<=79):
