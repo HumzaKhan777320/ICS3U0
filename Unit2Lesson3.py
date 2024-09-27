ef3882357c3b52ee22888f26a0005a38ef6d6b1e
HumzaKhan777320 <777320@pdsb.net>
2024-09-27 10:26:23 -0400

    Create Unit2Lesson3.py

---
+++ Unit2Lesson3.py
@@ -0,0 +1,8 @@
+x=1 #creating variable to store triangular number
+y=2 #creating variable to incriment to find triangular numbers
+z=1 #setting variable for while loop
+while(z<7): #creating while loop to print the first 6 triangular numbers
+  print("The #",z,"triangular number is", x) #outputing triangular numbers for user
+  x=x+y #calculating triangular numbers and storing in x
+  y+=1#incrementing y for next calculation
+  z+=1#incrementing z to at some point end while loop
