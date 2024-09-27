83ed6e50ef84323cbdd813625fd4212f44cc44f8
HumzaKhan777320 <777320@pdsb.net>
2024-09-27 10:53:06 -0400

    Create Unit2Lesson4.py

---
+++ Unit2Lesson4.py
@@ -0,0 +1,9 @@
+io = int(input("Enter a number to get factorials up to it: "))
+x=1 #creating variable to store factorials
+y=2 #creating variable to incriment to find factorials
+z=0 #setting variable for while loop
+while(z<io): #creating while loop to print factorials
+  print(z,"!=", x, sep = "") #outputing factorials for user
+  x=x*y #calculating factorials and storing in x
+  y+=1#incrementing y for next calculation
+  z+=1#incrementing z to at some point end while loop
