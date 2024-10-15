c85d33e9ea34ebc89ada54c77528422e478dd04b
HumzaKhan777320 <777320@pdsb.net>
2024-10-15 10:19:02 -0400

    Update Unit3Lesson9.py

--- Unit3Lesson9.py
+++ Unit3Lesson9.py
@@ -1,5 +1,4 @@
-#Not done yet
-def wc(TdegC, windKPH):
+def wc(TdegC, windKPH):#declaring a function that takes 2 parameters
    """
     * Calculates the wind chill, given
     * TdegC: the temp in degrees C
@@ -8,32 +7,32 @@ def wc(TdegC, windKPH):
     * Returns:
     * vTemp: Wind chill in degrees C
    """
-   vTemp = 0
+   vTemp = 0#creating variable to store the output(the wind chill)
 
-   vTemp=13.12+0.6215*TdegC-11.37*(windKPH**0.16)+((0.3965*TdegC)*windKPH**0.16)
+   vTemp=13.12+0.6215*TdegC-11.37*(windKPH**0.16)+((0.3965*TdegC)*windKPH**0.16)#doing calculations based on the function's parameters and storing in vTemp
 
-   return vTemp
+   return vTemp#returning the vTemp variable stored with calculations
 
-def myfunc(vTemp):
-  if(-9<=vTemp<=0):
-    return "Low risk"
-  if(-27<=vTemp<=-10):
-    return "Moderate risk of hypothermia"
-  if(-39<=vTemp<=-28):
-    return "High risk of frostbite"
-  if(-47<=vTemp<=-40):
-    return "Severe risk of frostbite: exposed skin freezes in 5-10 minutes"
-  if(-54<=vTemp<=-48):
-    return "Severe risk of frostbite: exposed skin freezes in 2-5 minutes"
-  if(vTemp<=-55):
-    return "Extreme risk: exposed skin freezes in under 2 minutes"
+def myfunc(vTemp):#declaring a function that takes 1 parameter for the risk statement 
+  if(-10<vTemp<=0):#using an if statment to check for 1st level of risk
+    return "Low risk"#returning coresponding statement
+  if(-28<vTemp<=-10):#using an if statment to check for 2nd level of risk
+    return "Moderate risk of hypothermia"#returning coresponding statement
+  if(-40<vTemp<=-28):#using an if statment to check for 3rd level of risk
+    return "High risk of frostbite"#returning coresponding statement
+  if(-48<vTemp<=-40):#using an if statment to check for 4th level of risk
+    return "Severe risk of frostbite: exposed skin freezes in 5-10 minutes"#returning coresponding statement
+  if(-55<vTemp<=-48):#using an if statment to check for 5th level of risk
+    return "Severe risk of frostbite: exposed skin freezes in 2-5 minutes"#returning coresponding statement
+  if(vTemp<=-55):#using an if statment to check for 6th level of risk
+    return "Extreme risk: exposed skin freezes in under 2 minutes"#returning coresponding statement
 
-T = -5.0
-W = 10.0
-print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))
-T = -20.0
-W = 20.0
-print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))
-T = -45.0
-W = 40.0
-print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))
+T = -5.0#setting a value of T
+W = 10.0#setting a value of W
+print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))#printing the wind chill with given wind speed and temp through wc and using the wind chill to find the risk through myfunc
+T = -20.0#setting a value of T
+W = 20.0#setting a value of W
+print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))#printing the wind chill with given wind speed and temp through wc and using the wind chill to find the risk through myfunc
+T = -45.0#setting a value of T
+W = 40.0#setting a value of W
+print("WC=%8.3f T=%8.3f W=%6.3f km/h. The risk is: %s" % (wc(T, W), T, W, myfunc(wc(T,W))))#printing the wind chill with given wind speed and temp through wc and using the wind chill to find the risk through myfunc
