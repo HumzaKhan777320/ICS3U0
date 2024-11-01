1df3f9f851ea363dbac0014c07a8da9541cddb63
HumzaKhan777320 <777320@pdsb.net>
2024-11-01 09:09:35 -0400

    Create Unit3Lesson11.py

---
+++ Unit3Lesson11.py
@@ -0,0 +1,22 @@
+#not done yet
+import random as r
+def shuffle(A):
+    # Precondition: A must be a 1D array
+    #
+    # Randomizes a list
+    
+    # add your code here!
+    
+    return A
+    
+
+size = int(input("Enter the size of the array"))
+B = []
+for i in range(1,size+1):
+  B.append(i)
+
+# add your code here!
+
+print(B) 
+B = shuffle(B)
+print(B) 
