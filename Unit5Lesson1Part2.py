8f3edf0a1cb256d7f7bd134cf3c24712c27cb688
HumzaKhan777320 <777320@pdsb.net>
2024-11-14 09:12:30 -0500

    Create Unit5Lesson1Part2.py

---
+++ Unit5Lesson1Part2.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+for i in range(80):
+    t.forward(100)
+    t.left(93)
+
+t.penup()
+t.goto(300,300)
+t.pendown()
+for i in range(60):
+  t.forward(100)
+  t.left(123)
