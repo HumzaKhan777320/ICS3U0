cff4c214ad84c08c60fab58c74d8a12096fa7517
HumzaKhan777320 <777320@pdsb.net>
2024-11-15 10:47:29 -0500

    Update Unit5Lesson2.py

--- Unit5Lesson2.py
+++ Unit5Lesson2.py
@@ -1,15 +1,32 @@
 #not done yet
-filename = "test.txt"
-fh = open(filename, "r") 
-numColors=3
-colorDefs = [[0] * 2] * numColors 
+filename = "file.txt"
+fh = open(filename, "r")
+colorLine = fh.readline() 
+colorLine.strip()
+row, col, num = colorLine.split()
+row=int(row)
+col=int(col)
+numColors=int(num)
+colorDefs = [[0] * numColors]*numColors
+print("There are",row,"rows")
+print("There are",col,"columns")
+print("There are",numColors,"colors")
 for i in range(numColors):
-   colorLine = fh.readline() 
-   colorLine.strip()
-   sym, c, color = colorLine.split()
-   colorDefs[i][0] = sym
-   colorDefs[i][1] = color
-   if(sym=="~"):
-       colorLine=colorLine.replace("~"," ")
-   print(colorLine)
+    colorLine = fh.readline() 
+    colorLine.strip()
+    sym, c, color = colorLine.split()
+    if(sym=="~"):
+        sym=" "
+    colorDefs[i][0] = sym
+    colorDefs[i][1] = color
+    print(colorDefs[i][0],":",colorDefs[i][1])
+x=0
+if(row<=col):
+    x=col
+else:
+    x=row
+a=[0]*x
+for i in range(x):
+    a[i]=fh.readline()
+    print(a[i],end="")
 fh.close()
