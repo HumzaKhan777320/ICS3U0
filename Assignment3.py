f33906d4862c313ca7775394e043ea61090082e2
HumzaKhan777320 <777320@pdsb.net>
2024-11-19 23:48:19 -0500

    Update and rename work.py to Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,10 +1,10 @@
+#not done yet
 import turtle
 
-t = turtle.Turtle()
 def plot(x,y,z,color):
     x+y+z+color
 
-filename = "cool_smiley_mod.xpm"
+filename = "test1.xpm"
 fh = open(filename, "r")
 colorLine = fh.readline() 
 colorLine.strip()
@@ -12,11 +12,11 @@ row, col, num = colorLine.split()
 row=int(row)
 col=int(col)
 numColors=int(num)
-colorDefs = [[0] * 2]*numColors
+colorDefs = [[0]*2]*numColors
 print("There are",row,"rows")
 print("There are",col,"columns")
 print("There are",numColors,"colors")
-c=[]
+c1=[[0]]*numColors
 for i in range(numColors):
     colorLine = fh.readline() 
     colorLine.strip()
@@ -25,21 +25,25 @@ for i in range(numColors):
         sym=" "
     colorDefs[i][0] = sym
     colorDefs[i][1] = color
-    #print(colorDefs[i][0],":",colorDefs[i][1])
-    print(colorDefs)
-
+    print(colorDefs[i][0],":",colorDefs[i][1])
+    c1[i]=[colorDefs[i][0],colorDefs[i][1]]
 x=0
 if(row<=col):
     x=col
 else:
     x=row
 a=[0]*x
-for i in range(x):
-    turtle.penup()
-    turtle.goto(x/2+i, x/2)
-    turtle.pendown()
-    turtle.dot(1,"blue")
-    a[i]=fh.readline()
-    print(a[i],end="")
-print(colorDefs)
+print(c1)
+for j in range(x):
+    for i in range(x):
+        if(j==0):
+            a[i]=fh.readline()
+            print(a[i],end="")
+        turtle.penup()
+        turtle.goto(x/2+(i*10), x/2-(j*10))
+        turtle.pendown()
+        for l in c1:
+            if l[0]==a[j][i]:
+                k=l[1]
+        turtle.dot(10,k)
 fh.close()
