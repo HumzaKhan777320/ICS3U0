ab1056f78632ae8fb85bb978d62c5ed0981a05da
HumzaKhan777320 <777320@pdsb.net>
2024-11-20 23:10:23 -0500

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,22 +1,47 @@
 #not done yet
-import turtle
-turtle.tracer(0,0)   
-def plot(x,y,z,color):
-    x+y+z+color
+import turtle   
 
-filename = "rocky_bullwinkle_mod.xpm"
+def plot(a,b,x,y,z,color):
+    turtle.tracer(0,0)
+    turtle.penup()
+    turtle.goto(-a*2/2+(x*2),b*2/2-(y*2))
+    turtle.pendown()
+    turtle.dot(z,color)
+    turtle.hideturtle()
+    
+def flip(a,b,x,y,z,color):
+    turtle.tracer(0,0)
+    turtle.penup()
+    turtle.goto(a*2/2-(x*2),-b*2/2+(y*2))
+    turtle.pendown()
+    turtle.dot(z,color)
+    turtle.hideturtle() 
+      
+def q(fh,row,col,z):
+    a=[0]*row
+    for j in range(row):
+        a[j]=fh.readline()
+        for t in range(col):
+            for l in c1:
+                if l[0]==a[j][t]:
+                    k=l[1]
+            z(row,col,t,j,3,k)
+    turtle.update()
+    
+filename = "rb.xpm"
 fh = open(filename, "r")
+
 colorLine = fh.readline() 
 colorLine.strip()
-row, col, num = colorLine.split()
+col, row, num = colorLine.split()
+
 row=int(row)
 col=int(col)
 numColors=int(num)
+
 colorDefs = [[0]*2]*numColors
-print("There are",row,"rows")
-print("There are",col,"columns")
-print("There are",numColors,"colors")
 c1=[[0]]*numColors
+
 for i in range(numColors):
     colorLine = fh.readline() 
     colorLine.strip()
@@ -25,25 +50,8 @@ for i in range(numColors):
         sym=" "
     colorDefs[i][0] = sym
     colorDefs[i][1] = color
-    print(colorDefs[i][0],":",colorDefs[i][1])
     c1[i]=[colorDefs[i][0],colorDefs[i][1]]
-x=0
-x=col
-y=row
-a=[0]*x
-for j in range(x):
-    for i in range(y):
-        if(j==0):
-            a[i]=fh.readline()
-        turtle.tracer(0,0)
-        turtle.penup()
-        turtle.goto(y*-3/2+(i*3), x*3/2-(j*3))
-        turtle.pendown()
-        for l in c1:
-            if l[0]==a[j][i]:
-                print(a)
-                k=l[1]
-        turtle.dot(3,k)
-        turtle.hideturtle()
-turtle.update()
+
+q(fh,row,col,plot)        
 fh.close()
+turtle.mainloop()
