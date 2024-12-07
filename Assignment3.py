833dc6895547f9ec5177a4130d35709e5d340e5f
HumzaKhan777320 <777320@pdsb.net>
2024-12-06 23:45:37 -0500

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,9 +1,11 @@
 """
-Name: Humza Saleem Khan
+Name: Humza Saleem Khan 
 Student Number: 777320@pdsb.net
 Course Code: ICS3U0
 Assignment: Assignment 3 - Making a graphics plotter in Turtle
-11/28/2024
+12/06/2024
+
+The header is located above including all the information
 
 Variable Dictionary:
 inputs - Function to get user inputs and append to various_inputs array
@@ -35,6 +37,7 @@ numColors - Number of color definitions in the file
 sym - Variable that stores a symbol corrisponding with a color code
 c - Variable needed for unpacking color definitions
 colorLine - Variable that stores a line from the file containing color information
+run_code - bool variable for controling the main while loop
 """
 import turtle   
 # importting turtle library
@@ -49,24 +52,34 @@ def inputs(various_inputs):
     usr_inpt=input(whole_prmpt)
     #displaying prompt to user
     #taking the file name as input from the user and storing in usr_inpt
-    thickness=int(input("\nEnter the desired thickness of the image as an integer >1: "))
-    #taking user input for the thichness of each point (size of image)
-    angle1=int(input("What angle rotation do you want the image 0, 90, 180, or 270: "))
-    #taking the angle of rotation from the user with provided prompt
-    if(angle1 != 0): #checking if the angle is not 0
-        if(angle1 != 90):#checking if thae angle is not 90
-            if(angle1 != 180):#checking if the angle is not 180
-                if(angle1 != 270): #checking if the angle is not 270
-                    print("Sorry this is not a valid angle so we'll default to 0")
-                    #telling the user they did not input a valid angle
-                    angle1=0
-                    #defaulting to a 0 degree angle
-    various_inputs.append(usr_inpt)
-    #appending inputed file name to various_inputs array for out of function access
-    various_inputs.append(thickness)
-    #appending inputed thickness to various_inputs array for out of function access
-    various_inputs.append(angle1)
-    #appending inputed angle to various_inputs array for out of functiopn access
+    try:
+        #declaring try function
+        thickness=int(input("\nEnter the desired thickness of the image as an integer >1: "))
+        #taking user input for the thichness of each point (size of image)
+        angle1=int(input("What angle rotation do you want the image 0, 90, 180, or 270: "))
+        #taking the angle of rotation from the user with provided prompt
+        if(angle1 != 0): #checking if the angle is not 0
+            if(angle1 != 90):#checking if thae angle is not 90
+                if(angle1 != 180):#checking if the angle is not 180
+                    if(angle1 != 270): #checking if the angle is not 270
+                        print("Sorry this is not a valid angle so we'll default to 0")
+                        #telling the user they did not input a valid angle
+                        angle1=0
+                        #defaulting to a 0 degree angle
+        various_inputs.append(usr_inpt)
+        #appending inputed file name to various_inputs array for out of function access
+        various_inputs.append(thickness)
+        #appending inputed thickness to various_inputs array for out of function access
+        various_inputs.append(angle1)
+        #appending inputed angle to various_inputs array for out of functiopn access
+    except:
+    #declaring an excapt function 
+        print("Sorry your input for either the thickness or angle of rotation is not an integer")
+        #outputting a message to the user that their inputs are of incorrect data type (not ints)
+        inputs(various_inputs)
+        #calling the inputs function to give the user a chance to reinput
+        #won't recall the function until its called in the driver code for
+        #the first time 
 
 def color_array(numColors,colorDefs,filtered_colorDefs):
     #declaring color_array function to read color information and store it
@@ -167,59 +180,67 @@ def plot(x,y,size,color,angle):
     turtle.hideturtle()
     #hiding the plotted point for faster plotting since the screen dosn't have to update yet       
     
-
-various_inputs=[]
-#declaring array various_inputs to use for the inputs function   
-inputs(various_inputs)
-#calling the inputs function with the various_inputs array as parameter 
-filename = various_inputs[0]
-#declaring variable filename and storing the 0th index of various_inputs array in it
-try:
-    fh = open(filename, "r")
-    #creating variable fh to open the filename file
-    colorLine = fh.readline()
-    #creating variable colorLine and storing the first line of the file in it 
-    colorLine.strip()
-    #getting rid of spaces in between the elements in colorLine
-    col = colorLine.split()[0]
-    #spliting colorLine and storing the 0th index of it in variable col  
-    row = colorLine.split()[1]
-    #spliting colorLine and storing the 1st index of it in variable row 
-    num = colorLine.split()[2]
-    #spliting colorLine and storing the 2nd index of it in variable num 
-    row=int(row)
-    #turning row into an int
-    col=int(col)
-    #turning col into an int
-    numColors=int(num)
-    #turning num into an int and storing in variable numColors
-    colorDefs = [[0]*2]*numColors
-    #declaring colorDefs 2D array with numColors internal arrays with 2 elements per array 
-    filtered_colorDefs=[[0]]*numColors
-    #declaring filtered_colorDefs array with numColors elements to store each color definition 
-    angle1=various_inputs[2]
-    #storing the 2nd index of the various_inputs array in variable angle1 to check how to
-    #rotate the image
-    thickness=various_inputs[1]
-    #storing the 1st index of the various_inputs array in variable thickness to have a constant
-    #size for each dot plotted
-    turtle.screensize(400,400,"white")
-    #setting the turtle screen size to 400X400 with white as the background color 
-    color_array(numColors,colorDefs,filtered_colorDefs)
-    #calling the color_array function with parameters int numColors, 2D array colorDefs
-    #and filtered_colorDefs array to store the color definitions
-    loops(row,col,thickness,angle1,filtered_colorDefs)
-    #calling the loops function with parameters ints row and col, int thickness, int angle1
-    #which is the angle of rotation and the filtered_colorDefs array to go through each symbol
-    #in the encoded image (from file) and call the plot function
-    print("\nYour turtle window is now ready check your taskbar")
-    #telling the user to check for the turtle window
-    fh.close()
-    #closing the file to prevent it from getting corrupted
-    turtle.mainloop()
-    #making the turtle window stay open
-except:
-    print("Sorry your file was not found please check your file's name and location",end="")
-    #printing a message to the user that there file name is wrong
-    print(" and try to run the code again")
-    #telling the user to try the code again after checking the file name and location
+run_code=True
+#declaring bool variable run_code for controling the main while loop and setting to True
+while(run_code==True):
+    #declaring while loop that works if run_code is True
+    various_inputs=[]
+    #declaring array various_inputs to use for the inputs function   
+    inputs(various_inputs)
+    #calling the inputs function with the various_inputs array as parameter 
+    filename = various_inputs[0]
+    #declaring variable filename and storing the 0th index of various_inputs array in it
+    try:
+    #declaring try function
+        fh = open(filename, "r")
+        #creating variable fh to open the filename file
+        colorLine = fh.readline()
+        #creating variable colorLine and storing the first line of the file in it 
+        colorLine.strip()
+        #getting rid of spaces in between the elements in colorLine
+        col = colorLine.split()[0]
+        #spliting colorLine and storing the 0th index of it in variable col  
+        row = colorLine.split()[1]
+        #spliting colorLine and storing the 1st index of it in variable row 
+        num = colorLine.split()[2]
+        #spliting colorLine and storing the 2nd index of it in variable num 
+        row=int(row)
+        #turning row into an int
+        col=int(col)
+        #turning col into an int
+        numColors=int(num)
+        #turning num into an int and storing in variable numColors
+        colorDefs = [[0]*2]*numColors
+        #declaring colorDefs 2D array with numColors internal arrays with 2 elements per array 
+        filtered_colorDefs=[[0]]*numColors
+        #declaring filtered_colorDefs array with numColors elements to store each color definition 
+        angle1=various_inputs[2]
+        #storing the 2nd index of the various_inputs array in variable angle1 to check how to
+        #rotate the image
+        thickness=various_inputs[1]
+        #storing the 1st index of the various_inputs array in variable thickness to have a constant
+        #size for each dot plotted
+        turtle.screensize(400,400,"white")
+        #setting the turtle screen size to 400X400 with white as the background color 
+        color_array(numColors,colorDefs,filtered_colorDefs)
+        #calling the color_array function with parameters int numColors, 2D array colorDefs
+        #and filtered_colorDefs array to store the color definitions
+        loops(row,col,thickness,angle1,filtered_colorDefs)
+        #calling the loops function with parameters ints row and col, int thickness, int angle1
+        #which is the angle of rotation and the filtered_colorDefs array to go through each symbol
+        #in the encoded image (from file) and call the plot function
+        print("\nYour turtle window is now ready check your taskbar")
+        #telling the user to check for the turtle window
+        fh.close()
+        #closing the file to prevent it from getting corrupted
+        turtle.mainloop()
+        #making the turtle window stay open
+        run_code=False
+        #setting run_code to False after plotting succesfully
+    except:
+    #declaring except function
+        print("Sorry your file was not found please check your file's name and location",end="")
+        #printing a message to the user that there file name is wrong
+        print(" and try again")
+        #telling the user to try again after checking the file name and location
+        
