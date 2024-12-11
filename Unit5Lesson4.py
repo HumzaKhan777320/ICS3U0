cc8d9675c82d25cf8962a79df922d56296dca806
HumzaKhan777320 <777320@pdsb.net>
2024-12-11 10:16:08 -0500

    Update Unit5Lesson4.py

--- Unit5Lesson4.py
+++ Unit5Lesson4.py
@@ -1,5 +1,14 @@
 a=open("words40.dat",'r')
 b=[]
 for i in range(40):
-    b.append(a.readline().replace('\n', ''))
-print(b)
+    b.append(a.readline().strip())
+for i in range(len(b)):
+    for j in range(i):
+        if(b[i]<b[j]):
+            b[i],b[j]=b[j],b[i]
+for i in range(1,5):
+    for k in range(1,11):
+        print(b[i*k-1], end = "  ")
+    print("\n")
+a.close()
+print(f"{len(b)} words.")
