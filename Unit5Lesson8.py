0dd0d1ee4cdfa5aa869dd7b36bebe05a118e7ec7
HumzaKhan777320 <777320@pdsb.net>
2024-12-16 06:29:52 -0500

    Update Unit5Lesson8.py

--- Unit5Lesson8.py
+++ Unit5Lesson8.py
@@ -1,17 +1,20 @@
-#not done
 def validate(N):
   total=0
   a=len(N)
   N=str(N)
-  for i in range(0,a+1,2):
-    print(i)
-    total+=int(N[a-i])*2
+  for i in range(0,a,2):
+    add=int(N[a-i-2])*2
+    if(i+2)<a:
+      if(add<10):
+        total+=add
+      else:
+        total+=add-9
   for i in range(0,a,2):
     total+=int(N[i])
   if(total%10==0):
-    isValid=True
+    return True
   else:
-    isValid=False
+    return False
 
 print("Validate a number with the Luhn Algorithm!")
 num = input("Input a number to validate: ")
